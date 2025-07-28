39b49e8211d7aedcc4e3463770c5809b7caefe78
AnassAntar04 <antaranass2004@gmail.com>
2025-07-28 14:56:26 +0100

    `Added console logs to iits.js file`

--- iits.js
+++ iits.js
@@ -1,2 +1,6 @@
 const iits ='internation it maroc ';
 console.log(iits);
+console.log('This is a test for the iits.js file.');
+console.log('Make sure to check the output for any errors or issues.');
+console.log('This file is part of the iits project.');
+console.log('Remember to keep the code clean and well-documented.');
